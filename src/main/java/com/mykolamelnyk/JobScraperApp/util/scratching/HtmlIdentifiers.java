c21c02087368d8b7dfe6245fa2c6728e2918fb6c
Mykola Melnyk <mykolamelnyk338@gmail.com>
2025-06-20 18:30:46 +0300

    Create HtmlIdentifiers

--- src/main/java/com/mykolamelnyk/JobScraperApp/util/scratching/HtmlIdentifiers.java
+++ src/main/java/com/mykolamelnyk/JobScraperApp/util/scratching/HtmlIdentifiers.java
@@ -1,23 +1,18 @@
-package com.mykolamelnyk.JobScraperApp;
+package com.mykolamelnyk.JobScraperApp.util.scratching;
 
-public interface HtmlIdentifiers {
+public final class HtmlIdentifiers {
+    private HtmlIdentifiers() {}
 
-    String JOB_LIST_ITEM_SELECTOR = "div[data-testid=job-list-item]";
-    String JOB_LIST_ITEM_LINK_ATTRIBUTE = "a[data-testid=job-title-link]";
-
-    String JOB_CONTENT_BLOCK_SELECTOR = "div[data-testid=content]";
-
-    String JOB_POSITION_NAME_SELECTOR = "div[data-testid=content] h2.sc-beqWaB.jqWDOR";
-    String JOB_ORGANIZATION_URL_SELECTOR = "div[data-testid=content] a[data-testid=button]";
-    String JOB_LOGO_LINK_SELECTOR = "div[data-testid=content] [data-testid=image]";
-    String JOB_ORGANIZATION_TITLE_SELECTOR = "div[data-testid=content] p.sc-beqWaB.bpXRKw";
-
-    String JOB_SUB_GROUP_ONE_SELECTOR = "div[data-testid=content] div.sc-beqWaB.sc-gueYoa.kDyJCZ.MYFxR div.sc-beqWaB.sc-gueYoa.dmdAKU.MYFxR";
-
-    //String JOB_LABOR_FUNCTION_SELECTOR = "div[data-testid=content] div.sc-beqWaB.sc-gueYoa.kDyJCZ.MYFxR div.sc-beqWaB.sc-gueYoa.dmdAKU.MYFxR div.sc-beqWaB.bpXRKw:nth-child(1)";
-    //String JOB_LOCATION_SELECTOR = "div[data-testid=content] div.sc-beqWaB.sc-gueYoa.kDyJCZ.MYFxR div.sc-beqWaB.sc-gueYoa.dmdAKU.MYFxR div.sc-beqWaB.bpXRKw:nth-child(2)";
-
-    String JOB_DESCRIPTION_SELECTOR = "div[data-testid=content] div[data-testid=careerPage]";
-    String JOB_TAG_SELECTOR = "div[data-testid=tag]";
+    public static final String JOB_LIST_ITEM_SELECTOR = "div[data-testid=job-list-item]";
+    public static final String JOB_LIST_ITEM_LINK_ATTRIBUTE = "a[data-testid=job-title-link]";
+    public static final String JOB_LIST_TOTAL_SIZE_SELECTOR = "#content div[data-testid=header] b";
+    public static final String JOB_POSITION_NAME_SELECTOR = "div[data-testid=content] h2.sc-beqWaB.jqWDOR";
+    public static final String JOB_ORGANIZATION_URL_SELECTOR = "div[data-testid=content] a[data-testid=button]";
+    public static final String JOB_LOGO_LINK_SELECTOR = "div[data-testid=content] [data-testid=image]";
+    public static final String JOB_ORGANIZATION_TITLE_SELECTOR = "div[data-testid=content] p.sc-beqWaB.bpXRKw";
+    public static final String JOB_SUB_GROUP_ONE_SELECTOR = "div[data-testid=content] div.sc-beqWaB.sc-gueYoa.kDyJCZ.MYFxR div.sc-beqWaB.sc-gueYoa.dmdAKU.MYFxR";
+    public static final String JOB_DESCRIPTION_SELECTOR = "div[data-testid=content] div[data-testid=careerPage]";
+    public static final String JOB_TAG_SELECTOR = "div[data-testid=tag]";
+    public static final String JOB_LIST_LOAD_MORE_BUTTON_SELECTOR = "#content div[data-testid=container] div[data-testid=results-list-job] div[data-testid=footer] button[data-testid=load-more]";
 
 }
